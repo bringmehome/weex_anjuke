e873515a895f4ec0850f25071e3261d42086f8ee
bringmehome <bringmehome@vip.qq.com>
2016-12-14 13:39:58 +0800

    [init]

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# weex_anjuke
-使用weex开发安居客的首页
+weex index.we --qr -h 192.168.3.199
+
+http://192.168.3.199:8081/weex_tmp/h5_render/
---
+++ js/iconapi.js
@@ -0,0 +1,6 @@
+exports.iconurl = {
+    resurl: 'http://www.easyicon.net/api/resizeApi.php?' //基地址
+}
+exports.appconfig={
+	linecolor:'#f6f7f8'
+}
